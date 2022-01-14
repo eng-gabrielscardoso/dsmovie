363dbdc0ebf5c7aa66165cd98580eda06a215968
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-01-14 09:00:44 -0300

    feat: movie stars

--- frontend/src/pages/listing/Listing.tsx
+++ frontend/src/pages/listing/Listing.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import Pagination from 'components/pagination/Pagination';
+import MovieStars from 'components/MovieStars/MovieStars';
 
 import './Listing.css';
 
 function Listing(){
   return(
-    <Pagination />
+    <>
+      <Pagination />
+      <MovieStars />
+    </>
   );
 };
 
