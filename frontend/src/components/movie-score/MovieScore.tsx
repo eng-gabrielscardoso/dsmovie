ea7e310380bfddac1a4983f9de746efd619b460f
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-01-16 16:40:10 -0300

    feat: show score

--- frontend/src/components/movie-score/MovieScore.tsx
+++ frontend/src/components/movie-score/MovieScore.tsx
@@ -4,14 +4,18 @@ import MovieStars from 'components/movie-stars/MovieStars';
 
 import './MovieScore.css';
 
-function MovieScore() {
-  const count = 4.5;
-  const score = 19;
+type Props = {
+  score: number;
+  count: number;
+};
+
+function MovieScore({ score, count }: Props) {
+  
 
   return(
     <div className="dsmovie-score-container">
       <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-      <MovieStars />
+      <MovieStars score={score} />
       <p className="dsmovie-score-count">{count} avaliações</p>
     </div>
   );
